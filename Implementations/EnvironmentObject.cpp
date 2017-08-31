b9d83eed5d2872f459ba62bdd9113867ee7bd4b0
Maxwell <maxwell.miller.freelance@gmail.com>
2017-08-30 19:21:14 -0600

    RenderText: Added destructor removed SetID call. EnvironmentObject: Removed SetID calls

--- Implementations/EnvironmentObject.cpp
+++ Implementations/EnvironmentObject.cpp
@@ -10,13 +10,11 @@ namespace KillerEngine
 	EnvironmentObject::EnvironmentObject(void)
 	{
 		GameObject2D::SetSprite(new SqrSprite());
-		GameObject2D::SetID();
 	}
 
 	EnvironmentObject::EnvironmentObject(KM::Vector2& pos, F32 w, F32 h)
 	{
 		GameObject2D::SetSprite(new SqrSprite());
-		GameObject2D::SetID();
 		GameObject2D::SetDimensions(w, h);
 		GameObject2D::SetPosition(pos);
 	}
@@ -24,9 +22,12 @@ namespace KillerEngine
 	EnvironmentObject::EnvironmentObject(KM::Vector2& pos, U32 textureID, F32 w, F32 h)
 	{
 		GameObject2D::SetSprite(new SqrSprite());
-		GameObject2D::SetID();
 		GameObject2D::SetDimensions(w, h);
 		GameObject2D::SetPosition(pos);
 		GameObject2D::SetTexture(textureID, 0.0f, 1.0f, 0.0f, 1.0f);
 	}
+
+	EnvironmentObject::~EnvironmentObject(void)
+	{  }
+	
 }//end namespace
