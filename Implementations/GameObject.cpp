37154e1179d46d8b81315689a160204b07ca68d1
maxwell <maxwell@layer8games.com>
2018-09-17 08:54:02 -0600

    Getting lighting to work

--- Implementations/GameObject.cpp
+++ Implementations/GameObject.cpp
@@ -127,7 +127,7 @@ void GameObject::v_InitBuffers(void)
 		if(vertNormals.size() > 0)
 		{
 			glBindBuffer(GL_ARRAY_BUFFER, _vbo[NORMAL_BUFFER]);
-			glBufferData(GL_ARRAY_BUFFER, (sizeof(F32) * vertTexCoords.size()), &vertTexCoords[0], GL_STATIC_DRAW);
+			glBufferData(GL_ARRAY_BUFFER, (sizeof(F32) * vertNormals.size()), &vertNormals[0], GL_STATIC_DRAW);
 			glVertexAttribPointer(NORMAL_POS, 4, GL_FLOAT, GL_FALSE, 0 , NULL);
 			glEnableVertexAttribArray(NORMAL_POS);				
 		}
@@ -254,6 +254,7 @@ bool GameObject::LoadOBJ(string filepath)
 					++dimension;
 				}
 
+				normal[3] = 0.0f;
 				normal.Normalize();
 				
 				tempNormals.push_back(normal);
