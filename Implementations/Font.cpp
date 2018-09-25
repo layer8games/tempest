98e492c629bebe0cd1f84feb2af392c91fbdf06e
maxwell <maxwell@layer8games.com>
2018-09-24 20:23:41 -0600

    Font: Began refactor of InitFont

--- Implementations/Font.cpp
+++ Implementations/Font.cpp
@@ -1,5 +1,7 @@
 #include <Engine/Font.h>
 
+#include <iostream>
+
 using namespace KillerEngine;
 
 //==========================================================================================================================
@@ -33,10 +35,36 @@ _characterData(f->GetCharacterData())
 //Functions
 //
 //==========================================================================================================================
-void Font::InitFont(string fontName, string fontFile)
+void Font::InitFont(string fontName, string filePath)
 {
-	
-		
+	if(filePath.find(".fnt") == string::npos)
+	{
+		ErrorManager::Instance()->SetError(EC_Engine, "Font::InitFont => Wrong file type: " + filePath);
+		return;
+	}
+
+	std::ifstream file(filePath);
+
+	if(!file)
+	{
+		ErrorManager::Instance()->SetError(EC_Engine, "Font::InitFont => unable to open file: " + filePath);
+		return;
+	}
+
+	std::vector<char> buffer((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+	buffer.push_back('\0');
+
+	file.close();
+
+	rapidxml::xml_document<char> doc;
+	doc.parse<0>(&buffer[0]);
+
+	rapidxml::xml_node<>* root_node = doc.first_node("font")->first_node("chars");
+
+	U32 totalChars = static_cast<U32>(std::stoi(root_node->first_attribute("count")->value()));
+
+	std::cout << "total chars = " << totalChars << "\n";
+
 }//InitFont
 
 CharacterData Font::GetCharacterData(char c)
@@ -45,13 +73,6 @@ CharacterData Font::GetCharacterData(char c)
 
 	return _characterData[id];
 
-}
-
-void Font::_AddNewCharacterData(string id,      string x, 		string y,
-						    	string width,   string height,  string xoffset,
-						    	string yoffset, string xadvance)
-{
-	//CharacterData data;
 }
 
 //==========================================================================================================================
