5104362f030a7edd8d93982265382d2d3eba76da
Maxwell <maxwell@layer8games.com>
2019-05-29 12:28:31 -0600

    AudioListener: implemented, not tested or used

--- Headers/Engine/AudioListener.h
+++ Headers/Engine/AudioListener.h
@@ -2,6 +2,10 @@
 
 //=====Engine Includes=====
 #include <Engine/Atom.h>
+#include <Engine/Vector3.h>
+#include <Engine/AudioManager.h>
+
+namespace KM = KillerMath;
 
 namespace KillerEngine
 {
@@ -19,10 +23,35 @@ namespace KillerEngine
 
 //==========================================================================================================================
 //
-//Functions
+//Accessors
 //
 //==========================================================================================================================
+		inline const F32* GetOrientation(void) const
+		{
+			return _orientation;
+		}
+
+		void SetOrientation(F32* orientation);
+
+		inline const KM::Vector3& GetPosition(void) const
+		{
+			return _position;
+		}
+
+		void SetPosition(const KM::Vector3& pos);
+
+		inline const KM::Vector3& GetVelocity(void) const
+		{
+			return _velocity;
+		}
+
+		void SetVelocity(const KM::Vector3 vel);
+
+
 	private:
-		
+		F32* 		_orientation;
+		KM::Vector3 _position;
+		KM::Vector3 _velocity;
+
 	};//end Class
 }//end Namespace
--- Implementations/AudioListener.cpp
+++ Implementations/AudioListener.cpp
@@ -7,7 +7,62 @@ using namespace KillerEngine;
 //
 //==========================================================================================================================
 AudioListener::AudioListener(void)
-{  }
+:
+_orientation(new F32[6]),
+_position(0.0f, 0.0f, 1.0f),
+_velocity(0.0f, 0.0f, 0.0f)
+{
+	AudioManager::Instance();
+
+	SetOrientation(_orientation);
+	SetPosition(_position);
+	SetVelocity(_velocity);
+}
 
 AudioListener::~AudioListener(void)
 {  }
+
+//==========================================================================================================================
+//
+//Accessors
+//
+//==========================================================================================================================
+
+void AudioListener::SetOrientation(F32* orientation)
+{
+	_orientation = orientation;
+
+	alListenerfv(AL_ORIENTATION, _orientation);
+
+	ALCenum error = alGetError();
+	if(error != AL_NO_ERROR)
+	{
+		ErrorManager::Instance()->SetError(AUDIO, "Oops! There was an error setting listener orientation\n");
+	}
+}
+
+void AudioListener::SetPosition(const KM::Vector3& pos)
+{
+	_position = pos;
+
+	alListener3f(AL_POSITION, _position[x], _position[y], _position[z]);
+	
+	ALCenum error = alGetError();
+	if(error != AL_NO_ERROR)
+	{
+		ErrorManager::Instance()->SetError(AUDIO, "Oops! There was an error setting the listener pos\n");
+	}
+}
+
+void AudioListener::SetVelocity(const KM::Vector3 vel)
+{
+	_velocity = vel;
+
+	alListener3f(AL_VELOCITY, _velocity[x], _velocity[y], _velocity[z]);
+
+	ALCenum error = alGetError();
+	if(error != AL_NO_ERROR)
+	{
+		ErrorManager::Instance()->SetError(AUDIO, "Oops! There was an error setting listener velocity\n");
+	}
+}
