1918a506cfa59a93ae0263555c1261e49a0b9895
Maxwell <maxwell@layer8games.com>
2019-05-29 21:45:15 -0600

    Created t_AudioManager. Need to write these tests, then plug into Boxes demo. Once that is done, all that is left is documentation for all of this...

---
+++ Tests/UnitTests/Implementations/t_AudioManager.cpp
@@ -0,0 +1,15 @@
+/*
+
+	Tests to add: 
+
+	AddClip
+	AddSource
+	RemoveClip
+	RemoveSource
+	AddClipToSource
+	SetListener
+	GetListener -> Change stuff
+	GetClip -> Change stuff
+	GetSource -> Change stuff 
+
+*/
