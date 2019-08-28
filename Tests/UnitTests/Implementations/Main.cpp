7ef53b720d5da69ecbbc2c6bc9a1d8fa7511fd6c
Maxwell <maxwell@layer8games.com>
2019-08-28 09:13:50 -0600

    Updated gitignore to ignore builds. Working on release version of build

--- Tests/UnitTests/Implementations/Main.cpp
+++ Tests/UnitTests/Implementations/Main.cpp
@@ -39,7 +39,11 @@ Written by Maxwell Miller
 //Required file. Simply here to get the test framework set up.
 //
 //==========================================================================================================================
-#pragma comment(lib, "Killer_Engine_Win32_Debug.lib")
+#if DEBUG
+	#pragma comment(lib, "Killer_Engine_Win32_Debug.lib")
+#else
+	#pragma comment(lib, "Killer_Engine_Win32_Release.lib")
+#endif
 
 #define BOOST_TEST_MAIN KillerUnitTests
 
