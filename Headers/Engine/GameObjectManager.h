c5055336e17454aa3d34ac19029f801b9a2c29cd
Maxwell Miller <maxwell@layer8games.com>
2020-01-26 21:13:42 -0700

    Merged conflicted files

--- Headers/Engine/GameObjectManager.h
+++ Headers/Engine/GameObjectManager.h
@@ -5,11 +5,11 @@
 
 namespace Tempest
 {
-/// A general manager class for all GameObject that are created. In the past, there have been a lot of issues with the way that
-/// GameObjects are stored. In the past, this was done in the Level, but this creates issues with persistence and ownership. 
-/// The purpose of this manager is to simplify this by registering all GameObjects in the GameObjectManager, using their ID 
-/// as their lookup key. The Level will then store just the ID of the GameObjects that it cares about, or that it registered, 
-/// and will look these up for rendering and updating. Only persisten, multilevel objects should be registered in this way. 
+	/// A general manager class for all GameObject that are created. In the past, there have been a lot of issues with the way that
+	/// GameObjects are stored. In the past, this was done in the Level, but this creates issues with persistence and ownership. 
+	/// The purpose of this manager is to simplify this by registering all GameObjects in the GameObjectManager, using their ID 
+	/// as their lookup key. The Level will then store just the ID of the GameObjects that it cares about, or that it registered, 
+	/// and will look these up for rendering and updating. Only persisten, multilevel objects should be registered in this way. 
 	class GameObjectManager
 	{
 	public:
