494dfe69e4ebad83bc6092325106ff8ac38d3672
Maxwell Miller <maxwell@layer8games.com>
2020-02-04 22:12:55 -0700

    Added code for StateMachine and State. I've tried to use shared_ptr, but this may not work. Need to create UnitTests to see

--- Headers/Engine/State.h
+++ Headers/Engine/State.h
@@ -5,6 +5,12 @@
 
 namespace Tempest
 {
+	/// A State is used in a StateMachine to define behavior at run time. This is based on the pattern that is well established as 
+	/// a good way to control what an object is going, given a certain context. These States should be used to express how a GameObject
+	/// should behave, given a certain context. This is an abstract class. It is also templated. This is so that any type of object
+	/// can have states added to it, and any of their functions can be called. Without this, only the functions located in the 
+	/// GameObject base could be called, and this would not do. This class also uses what is called an alias declaration template.
+	/// This allowed me to declare a shared_ptr to the template State. Very useful.
 	template <class GameObjectType>
 	class State
 	{
@@ -14,16 +20,33 @@ namespace Tempest
 		//Constructors	 	
 		//
 		//==========================================================================================================================
-		State(void);
+		/// No implementation.
+		State(void)
+		{  }
 
-		~State(void);
+		/// No implementation.
+		virtual ~State(void)
+		{  }
 
 		//==========================================================================================================================
 		//
 		//Functions
 		//
 		//==========================================================================================================================
+		/// Called when the State is made active by the StateMachine. Abstract, but be implemented for use.
+		/// \param object type is a defined GameObject which functions can and probably should be called on.
+		virtual void v_Enter(shared_ptr<GameObjectType> object)=0;
+
+		/// Called when the State is executed during the Update loop, by the StateMachine.
+		/// \param object type is a defined GameObject which functions can and probably should be called on.
+		virtual void v_Execute(shared_ptr<GameObjectType> object)=0;
+
+		/// Called when the State is finished. This could define what the next state is. 
+		/// \param object type is a defined GameObject which functions can and probably should be called on.
+		virtual void v_Exit(shared_ptr<GameObjectType> object)=0;
 	private:
 
 	};//end Class
+	template <class GameObjectType>
+	using p_State = shared_ptr<State<GameObjectType>>;
 }
--- Headers/Engine/StateMachine.h
+++ Headers/Engine/StateMachine.h
@@ -3,6 +3,7 @@
 //=====Engine Includes=====
 #include <Engine/Atom.h>
 #include <Engine/State.h>
+#include <Engine/ErrorManager.h>
 
 namespace Tempest
 {
@@ -15,18 +16,101 @@ namespace Tempest
 		//Constructors	 	
 		//
 		//==========================================================================================================================
-		StateMachine(void);
+		StateMachine(shared_ptr<GameObjectType> owner)
+			:
+			_owner(owner),
+			_currentState(nullptr),
+			_previousState(nullptr),
+			_globalState(nullptr)
+		{  }
 
-		~StateMachine(void);
+		~StateMachine(void)
+		{  }
 
 		//==========================================================================================================================
 		//
 		//Functions
 		//
 		//==========================================================================================================================
+		void Update(void) const
+		{
+			if(_globalState)
+			{
+				_globalState->v_Execute(_owner);
+			}
+			
+			if(_currentState)
+			{
+				_currentState->v_Execute(_owner);
+			}
+		}
+
+		void ChangeState(p_State<GameObjectType> newState)
+		{
+			if(newState == nullptr)
+			{
+				ErrorManager::Instance()->SetError(ENGINE, "StateMachine::ChangeState cannot pass nullptr to ChangeState!");
+				return;
+			}
+
+			_previousState = _currentState;
+
+			_currentState->v_Exit(_owner);
+
+			_currentState = newState;
+
+			_currentState->v_Enter(_owner);
+		}
+
+		void RevertState(void)
+		{
+			ChangeState(_previousState);
+		}
+
+
+
+		//==========================================================================================================================
+		//
+		//Accessors
+		//
+		//==========================================================================================================================
+		void SetCurrentState(p_State<GameObjectType> state)
+		{
+			_currentState = state;
+		}
+
+		p_State<GameObjectType> GetCurrentState(void) const
+		{
+			return _currentState;
+		}
+
+		void SetPreviousState(p_State<GameObjectType> state)
+		{
+			_previousState = state;
+		}
+
+		p_State<GameObjectType> GetPreviousState(void) const
+		{
+			return _previousState;
+		}
+
+		void SetGlobalState(p_State<GameObjectType> state)
+		{
+			_globalState = state;
+		}
+
+		p_State<GameObjectType> GetGlobalState(void) const
+		{
+			return _globalState;
+		}
+	
 	private:
 		shared_ptr<GameObjectType> _owner;
-		shared_ptr<State> _currentState;
+		p_State<GameObjectType> _currentState;
+		p_State<GameObjectType> _previousState;
+		p_State<GameObjectType> _globalState;
 
 	};//end Class
+	template <class GameObjectType>
+	using p_StateMachine = shared_ptr<StateMachine<GameObjectType>>;
 }
